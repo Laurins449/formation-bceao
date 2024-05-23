302063f97e388a69c83bc095ced9a25be647889b
Laurins449 <duraclenis@gmail.com>
2024-05-23 15:47:39 +0000

    first commit

---
+++ src/test/java/com/bceao/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bceao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
